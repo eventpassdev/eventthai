dff61cb10344148f3680b7f101cf75901a3d545a
Tanut2539 <tanut.leel@gmail.com>
2018-05-09 14:03:10 +0700

    [+] add 404 landing page

--- src/pages/404.js
+++ src/pages/404.js
@@ -5,11 +5,11 @@ import './NotFound.scss';
 export default class HTML extends React.Component {
     render() {
         return (
-            <section className="hero is-medium has-text-centered">
+            <section className="hero is-fullheight">
                 <div className="hero-body">
                     <div className="container">
-                        <div className="main main--notfound">
-                            <h3 className="title is-3">ขออภัย ไม่พบหน้าที่กำลังค้นหา</h3>
+                        <div className="main main--notfound has-text-centered">
+                            <h3 className="title is-3 has-text-white">ขออภัย ไม่พบหน้าที่กำลังค้นหา</h3>
                             <a className="button is-black is-rounded" href="/">กลับไปหน้าแรก</a>
                         </div>
                     </div>
